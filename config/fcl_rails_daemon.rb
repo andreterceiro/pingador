041022c29e66e2bb7deffd9882cd1b5e4e141cc9
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2021-01-18 23:49:13 -0300

    feat: base of fcl_rails_daemon

---
+++ config/fcl_rails_daemon.rb
@@ -0,0 +1,6 @@
+# To register commands use the FclRailsDaemon::Recorder class
+#  :command is the command name
+#  :class_reference is the class to which it is
+#
+# FclRailsDaemon::Recorder.add(command: 'command_sample', class_reference: CommandSample)
+
